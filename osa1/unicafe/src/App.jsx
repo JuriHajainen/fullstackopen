aeb391c2eaeaed655cccf965b61569a8b57c905b
Juri Hajainen <juri@vakuutustiedot.fi>
2025-05-20 09:34:00 +0300

    1.8 is done

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState/*, useEffect*/ } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
@@ -11,51 +11,106 @@ const Button = (props) => <button onClick={ props.onClick }>{ props.text }</butt
 
 //--- ------------------------------------------------
 //--- ------------------------------------------------
+const Statistics = (props) => {
+  return (
+  <div>
+    <h2>Statistics</h2>
+    <div>{ props.names[0] + ' ' + props.good }</div>
+    <div>{ props.names[1] + ' ' + props.neutral }</div>
+    <div>{ props.names[2] + ' ' + props.bad }</div>
+    <div>ALL: { props.all }</div>
+    <div>Average: { props.average }</div>
+    <div>Positive: { props.goodInPercentage } %</div>
+  </div>
+  )
+}
+
+//--- ------------------------------------------------
+//--- ------------------------------------------------
 const App = () => {
 
-  const names = ['Good', 'Neutral', 'Bad']
+  const names        = ['Good', 'Neutral', 'Bad']
+  const increasement = 1
 
-  const [good, setGood]       = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad]         = useState(0)
+  const [good, setGood]                         = useState(0)
+  const [neutral, setNeutral]                   = useState(0)
+  const [bad, setBad]                           = useState(0)
+  const [all, setAll]                           = useState(0)
+  const [average, setAverage]                   = useState(0)
+  const [goodInPercentage, setGoodInPercentage] = useState(0)
+
+
+  // useEffect(() => {
+  //   setAll(good + neutral + bad)
+  //   setAverage( ((good - bad) / all).toFixed(2) )
+  // }, [all, good, bad, neutral])
 
   
-  //--- METHODs ----------------------------
+  //--- METHODs ---------------------------------------
+  
+  //--- Button Click event handler --------------------
   const button_onClick = (status) => {
+    let increasementSet // For fixing asyn update of all etc
     return () => {
       switch (status) {
         case names[0]:
-          setGood(good + 1)
+          setGood(good + increasement)
+          increasementSet = 1 // For fixing asyn update of all etc
           break
-        case names[1]:
-          setNeutral(neutral + 1)
+          case names[1]:
+            setNeutral(neutral + increasement)
+          increasementSet = 0 // For fixing asyn update of all etc
           break
         case names[2]:
-          setBad(bad + 1)
+          setBad(bad + increasement)
+          increasementSet = -1 // For fixing asyn update of all etc
           break
         default:
           console.log('ERROR: button_onClick switch status is not defined: ', status)
           break
-      }
+        }
+        feedback_allSet()
+        feedback_averageSet(increasementSet)
+        feedback_goodInPercentageSet(status)
     }
   }
 
+  //--- Count ALL Feedbacks ------------------------
+  const feedback_allSet = () => {
+    setAll(good + neutral + bad + increasement)
+  }
+  
+  //--- Count average ------------------------
+  const feedback_averageSet = (increasementSet) => {
+    // console.log('ALL: ', all)
+    setAverage( ((good - bad + increasementSet) / (all + increasement)).toFixed(2) )
+  }
+
+  //--- Count "good" percentage from total ------------------------
+  const feedback_goodInPercentageSet = (status) => {
+    setGoodInPercentage((100 * (good + (status === 'Good' ? increasement : 0)) / (all + increasement)).toFixed(2))
+  }
+  
   
   //----------------------------------------
   return (
     <div>
+
       <div>
         <h2>Give feedback</h2>
         <Button onClick = { button_onClick(names[0]) } text = { names[0] } />
-        <Button onClick = { button_onClick(names[1]) } text = { names[1] }/>
-        <Button onClick = { button_onClick(names[2]) } text = { names[2] }/>
+        <Button onClick = { button_onClick(names[1]) } text = { names[1] } />
+        <Button onClick = { button_onClick(names[2]) } text = { names[2] } />
       </div>
-      <div>
-        <h2>Statistics</h2>
-        <div>{ names[0] + ' ' + good }</div>
-        <div>{ names[1] + ' ' + neutral }</div>
-        <div>{ names[2] + ' ' + bad }</div>
-      </div>
+
+      <Statistics names            = { names }
+                  good             = { good } 
+                  neutral          = { neutral } 
+                  bad              = { bad } 
+                  all              = { all } 
+                  average          = { average } 
+                  goodInPercentage = { goodInPercentage }  />
+
     </div>
   )
 }
